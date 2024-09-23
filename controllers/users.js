2b9758379d6cca0fc4041c020e399c7496858649
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-09-23 19:24:24 +0300

    controller const=user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const prisma = require("../prisma/prisma-client");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -7,7 +9,24 @@ const login = async (req, res) => {
     return res.status(400).json({ msg: "Please enter all fields" });
   }
 
-  const user = await prisma.user;
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  }); //this will return the first user that matches the email because using findFirst
+
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password)); //compare the password from the request with the password from the user in the database
+
+  if (user && isPasswordCorrect) {
+    response.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    }); //return the user data
+  } else {
+    response.status(400).json({ msg: "Incorrect login or password entered" });
+  }
 };
 
 const register = async (req, res) => {
