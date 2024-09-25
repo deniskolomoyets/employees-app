b4d184ef929345cb4e2d09257d0b555337260588
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-09-25 18:00:34 +0300

    added try/catch in function

--- controllers/users.js
+++ controllers/users.js
@@ -9,32 +9,36 @@ const jwt = require("jsonwebtoken");
  * @access Public
  */
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ msg: "Please enter all fields" });
-  }
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Please enter all fields" });
+    }
 
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  }); //this will return the first user that matches the email because using findFirst
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    }); //this will return the first user that matches the email because using findFirst
 
-  const isPasswordCorrect =
-    user && (await bcrypt.compare(password, user.password)); //compare the password from the request with the password from the user in the database
+    const isPasswordCorrect =
+      user && (await bcrypt.compare(password, user.password)); //compare the password from the request with the password from the user in the database
 
-  const secret = process.env.JWT_SECRET;
+    const secret = process.env.JWT_SECRET;
 
-  if (user && isPasswordCorrect && secret) {
-    res.status(200).json({
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      token: jwt.sign({ id: user.id }, secret, { expiresIn: "1d" }),
-    }); //return the user data
-  } else {
-    res.status(400).json({ msg: "Incorrect login or password entered" });
+    if (user && isPasswordCorrect && secret) {
+      res.status(200).json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        token: jwt.sign({ id: user.id }, secret, { expiresIn: "1d" }),
+      }); //return the user data
+    } else {
+      res.status(400).json({ msg: "Incorrect login or password entered" });
+    }
+  } catch (error) {
+    res.status(400).json({ msg: "Something go wrong" });
   }
 };
 
@@ -45,43 +49,47 @@ const login = async (req, res) => {
  * @access Public
  */
 const register = async (req, res, next) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    return res.status(400).json({ msg: "Please enter all fields" });
-  }
+    if (!name || !email || !password) {
+      return res.status(400).json({ msg: "Please enter all fields" });
+    }
 
-  const registeredUser = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  }); //check if the user already exists
-  if (registeredUser) {
-    return res.status(400).json({ msg: "User already exists" });
-  }
+    const registeredUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    }); //check if the user already exists
+    if (registeredUser) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
 
-  const salt = await bcrypt.genSalt(10); //generate the salt
-  const hashedPassword = await bcrypt.hash(password, salt); //hash the password
+    const salt = await bcrypt.genSalt(10); //generate the salt
+    const hashedPassword = await bcrypt.hash(password, salt); //hash the password
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  }); //create the user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    }); //create the user
 
-  const secret = process.env.JWT_SECRET; //get the secret from the environment variables
+    const secret = process.env.JWT_SECRET; //get the secret from the environment variables
 
-  if (user && secret) {
-    res.status(201).json({
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      token: jwt.sign({ id: user.id }, secret, { expiresIn: "1d" }), // sign the token with the user id and the secret and set the expiration time to 1 day
-    });
-  } else {
-    res.status(400).json({ msg: "Failed to create a user" });
+    if (user && secret) {
+      res.status(201).json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        token: jwt.sign({ id: user.id }, secret, { expiresIn: "1d" }), // sign the token with the user id and the secret and set the expiration time to 1 day
+      });
+    } else {
+      res.status(400).json({ msg: "Failed to create a user" });
+    }
+  } catch (error) {
+    res.status(400).json({ msg: "Something go wrong" });
   }
 };
 
