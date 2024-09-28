226b6c2c6604cecac5a953201ba12cc41b538f5e
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-09-28 17:16:43 +0300

    created Layout

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Layout } from "../../components/layout";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Layout>
+      <div>Login</div>
+    </Layout>
+  );
 };
