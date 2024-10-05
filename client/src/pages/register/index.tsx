2b3ba85c82d32c15d0f70655b875961dbef982d0
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-10-05 18:14:02 +0300

    Registration page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { Layout } from "../../components/layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { CustomInput } from "../../components/custom-input";
+import { PasswordInput } from "../../components/password-input";
+import { CustomButton } from "../../components/custom-button";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
 
 export const Register = () => {
-  return <div>register</div>;
+  return (
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Register" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput name="name" placeholder="Name" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+
+            <PasswordInput name="password" placeholder="Password" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Repeat the password"
+            />
+            <CustomButton type="primary" htmlType="submit">
+              Register
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Already have an account? <Link to={Paths.login}>Log in</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  );
 };
